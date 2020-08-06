82ed15d687abb7e7c4775ff48402b43308b80659
Pavol Pitonak <ppitonak@redhat.com>
2020-08-06 11:13:18 +0200

    helper scripts improved

--- get-list-of-images.sh
+++ get-list-of-images.sh
@@ -6,7 +6,7 @@ for CT in $CLUSTERTASKS; do
   CT=${CT%%/*}
   echo "--- $CT ---"
   #echo $(oc get clustertask $CT -o yaml | grep "image-registry\|gcr\|access.redhat.com\|quay.io")
-  echo $(oc get clustertask $CT -o yaml | grep "image-registry\|gcr\|access.redhat.com\|redhat.io\|quay.io")
+  echo $(oc get clustertask $CT -o yaml | yq '.spec' | grep "image-registry\|gcr\|access.redhat.com\|redhat.io\|quay.io")
   echo
 done
 
